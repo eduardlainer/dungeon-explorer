0fc68a120cef1f337205e5d92d3fff99489d87b1
edlainer <eduardlainer@gmail.com>
2019-04-16 22:10:44 +0300

    Refactor code from DungeonView and create a class for character and it's methods

---
+++ app/src/main/java/com/example/dungeonexplorer/Character.java
@@ -0,0 +1,103 @@
+package com.example.dungeonexplorer;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+
+class Character {
+
+    private final int CHARACTER_SIZE;
+    private final int MOVE_RATIO = 10;
+
+    private Bitmap chibiSprite;
+    private Rect sourceRect;
+    private Rect destinationRect;
+    private Paint emptyPaint;
+
+    private int movingFramesCount;
+    private int dx, dy;
+
+    Character(int CHARACTER_SIZE, Bitmap chibiSprite, Rect sourceRect, Rect destinationRect, Paint emptyPaint, int movingFramesCount) {
+        this.CHARACTER_SIZE = CHARACTER_SIZE;
+        this.chibiSprite = chibiSprite;
+        this.sourceRect = sourceRect;
+        this.destinationRect = destinationRect;
+        this.emptyPaint = emptyPaint;
+        this.movingFramesCount = movingFramesCount;
+    }
+
+    void drawCharacter(Canvas canvas) {
+        canvas.drawBitmap(chibiSprite,
+                sourceRect,
+                destinationRect,
+                emptyPaint
+        );
+    }
+
+    private void characterMoveLeft(int currentFrame) {
+        sourceRect = new Rect(currentFrame * CHARACTER_SIZE,
+                9 * CHARACTER_SIZE,
+                (currentFrame + 1) * CHARACTER_SIZE,
+                10 * CHARACTER_SIZE);
+    }
+
+    private void characterMoveRight(int currentFrame) {
+        sourceRect = new Rect(currentFrame * CHARACTER_SIZE,
+                11 * CHARACTER_SIZE,
+                (currentFrame + 1) * CHARACTER_SIZE,
+                12 * CHARACTER_SIZE);
+    }
+
+
+    private void characterMoveBottom(int currentFrame) {
+        sourceRect = new Rect(currentFrame * CHARACTER_SIZE,
+                10 * CHARACTER_SIZE,
+                (currentFrame + 1) * CHARACTER_SIZE,
+                11 * CHARACTER_SIZE);
+    }
+
+    private void characterMoveTop(int currentFrame) {
+        sourceRect = new Rect(currentFrame * CHARACTER_SIZE,
+                8 * CHARACTER_SIZE,
+                (currentFrame + 1) * CHARACTER_SIZE,
+                9 * CHARACTER_SIZE);
+    }
+
+    void move(String direction, int currentFrame, int viewWidth, int viewHeight) {
+
+        switch (direction) {
+            case "right":
+                characterMoveRight(currentFrame);
+                dx = destinationRect.right < viewWidth ? MOVE_RATIO : 0;
+                dy = 0;
+                break;
+            case "left":
+                characterMoveLeft(currentFrame);
+                dx = destinationRect.left > 0 ? -MOVE_RATIO : 0;
+                dy = 0;
+                break;
+            case "top":
+                characterMoveTop(currentFrame);
+                dx = 0;
+                dy = destinationRect.top > 0 ? -MOVE_RATIO : 0;
+                break;
+            case "bottom":
+                characterMoveBottom(currentFrame);
+                dx = 0;
+                dy = destinationRect.bottom < viewHeight ? MOVE_RATIO : 0;
+                break;
+        }
+
+        destinationRect.left += dx;
+        destinationRect.right += dx;
+        destinationRect.top += dy;
+        destinationRect.bottom += dy;
+    }
+
+    int getMovingFramesCount() {
+        return movingFramesCount;
+    }
+
+
+}
